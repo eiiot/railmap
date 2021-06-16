8f0522c3e5103c87703c52a0ee81318b063c545d
Eliot Hertenstein <39346975+EliotHertenstein@users.noreply.github.com>
2021-06-15 17:58:29 -0700

    Bug Fixes

--- europe/bundle-data-europe.js
+++ europe/bundle-data-europe.js
@@ -87,7 +87,7 @@ function aboutMap() {
 
 function switchLocation() {
   console.log("Location Switched");
-  window.location.href = 'http://www.therailmap.com/data;
+  window.location.href = 'http://www.therailmap.com/data';
 };
 
 map.on('load', function () {
