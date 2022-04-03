57534ce900cd2850161b110b1481ec908abd9f24
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-04-03 01:56:57 -0700

    fixed everything :+1:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import 'mapbox-gl-controls/lib/controls.css'
 import '@fontsource/inter'
 import type { AppProps } from 'next/app'
